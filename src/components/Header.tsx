8e5f14eae4597514ddcb5b2f260c7e1e44ca23fc
Emanuel Junior <emanuel.adm10@gmail.com>
2025-07-25 18:13:33 -0300

    Alterada a head section do projeto com o logo e os menus

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,18 +1,18 @@
 import { Button } from "@/components/ui/button";
-import logo from "@/assets/logo.png";
+import logo from "@/assets/logotipo.png";
 
 const Header = () => {
   return (
     <header className="fixed top-0 w-full bg-background/95 backdrop-blur-sm border-b border-border z-50">
       <div className="container mx-auto px-4 h-16 flex items-center justify-between">
         <div className="flex items-center gap-3">
-          <img src={logo} alt="Brincar Educando" className="h-10 w-10 rounded-lg" />
-          <div>
+          <img src={logo} alt="Brincar Educando" className="h-40 w-40 rounded-lg" />
+          {/*<div>
             <h1 className="text-xl font-semibold text-foreground">
               Brincar Educando
             </h1>
             <p className="text-xs text-muted-foreground">Cuidar brincando</p>
-          </div>
+          </div>*/}
         </div>
         
         <nav className="hidden md:flex items-center gap-6">
@@ -20,10 +20,16 @@ const Header = () => {
             Início
           </a>
           <a href="#sobre" className="text-foreground hover:text-primary transition-smooth">
-            Sobre
+            Top Recomendações
           </a>
           <a href="#resenhas" className="text-foreground hover:text-primary transition-smooth">
-            Resenhas
+            Guias por Idade
+          </a>
+          <a href="#contato" className="text-foreground hover:text-primary transition-smooth">
+            Blog
+          </a>
+          <a href="#contato" className="text-foreground hover:text-primary transition-smooth">
+            Sobre nós
           </a>
           <a href="#contato" className="text-foreground hover:text-primary transition-smooth">
             Contato
